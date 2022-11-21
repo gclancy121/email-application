671684ca36e74c099a7b6821dea241dec1e0abf0
Gage Clancy <gclancy121@gmail.com>
2022-11-21 12:44:03 -0600

    created working constructor

--- src/src/Email.java
+++ src/src/Email.java
@@ -1,5 +1,26 @@
 public class Email {
-    public static void main(String[] args) {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String altEmail;
 
+    //Constructor to receive the first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
     }
+
+    //Ask for department
+
+    //Generate random password
+
+    //Set the mailbox capacity
+
+    //Set the alt email
+
+    //Change password
+
 }
