33c05af88ff689048b6d4662d0bf3285850d6fbe
Gage Clancy <gclancy121@gmail.com>
2022-11-21 13:07:43 -0600

    created setDepartment method

--- src/src/Email.java
+++ src/src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -11,9 +13,24 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        //Call a method asking for department, return department
+        this.department = setDepartment();
+        System.out.println(this.department);
+
+        //Call a method that returns a random password
     }
 
     //Ask for department
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "sales"; }
+        else if (depChoice == 2) { return "dev"; }
+        else if (depChoice == 3) { return "accounting"; }
+        else {return ""; }
+    }
 
     //Generate random password
 
